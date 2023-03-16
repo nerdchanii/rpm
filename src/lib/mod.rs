fb3f8e118fad56f8cb3461fb87ab562f1a7cc7f1
nerdchanii <nerdchanii@gmail.com>
2023-03-16 15:12:54 +0900

    feat: add Pcakge.json struct

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,6 +1,7 @@
 pub mod api;
 pub mod command;
 pub mod opt;
+pub mod packge_json;
 pub mod parser;
 pub mod registry;
 pub mod util;
---
+++ src/lib/packge_json/mod.rs
@@ -0,0 +1,139 @@
+use serde::{Deserialize, Serialize};
+use serde_json::to_writer_pretty;
+use std::{
+    collections::HashMap,
+    fs::{self, OpenOptions},
+    io::BufWriter,
+    path::Path,
+};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct VersionString(String);
+
+impl VersionString {
+    fn new(version: String) -> VersionString {
+        VersionString(version)
+    }
+
+    fn to_string(&self) -> String {
+        self.0.to_owned()
+    }
+
+    fn to_owned(&self) -> VersionString {
+        VersionString(self.0.to_owned())
+    }
+
+    fn to_str(&self) -> &str {
+        &self.0
+    }
+
+    fn to_specific_version(&self) -> String {
+        let mut version = self.0.to_owned();
+        if version.starts_with("^") {
+            version = version.replace("^", "");
+        }
+        if version.starts_with("~") {
+            version = version.replace("~", "");
+        }
+        version
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Package {
+    name: String,
+    version: VersionString,
+    // main type will be changed PathString
+    main: String,
+    author: String,
+    // will be changed License Enum
+    license: String,
+    #[serde(default = "HashMap::new")]
+    dependencies: HashMap<String, VersionString>,
+    #[serde(rename = "devDependencies", skip_serializing_if = "Option::is_none")]
+    dev_dependecies: Option<HashMap<String, VersionString>>,
+    // other fields implement soon.
+}
+
+impl Package {
+    fn new() -> Package {
+        let text = fs::read_to_string("./package.json").unwrap_or("".to_owned());
+        let package: Package = serde_json::from_str(&text).unwrap();
+        package
+    }
+
+    fn read_file() -> Package {
+        let text = fs::read_to_string("./package.json").unwrap_or("".to_owned());
+        let package: Package = serde_json::from_str(&text).unwrap();
+        package
+    }
+
+    fn save(&self) -> core::result::Result<(), ()> {
+        let package_json_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(Path::new("./package.json"))
+            .unwrap();
+
+        // save serialized $self to package.json
+        if let Ok(ser) = serde_json::to_value(self) {
+            let writer = &mut BufWriter::new(package_json_file);
+            println!("{}", &ser);
+            let result = to_writer_pretty(writer, &ser);
+            if result.is_ok() {
+                Ok(())
+            } else {
+                Ok(())
+            }
+        } else {
+            panic!("error");
+        }
+    }
+
+    fn add_dependecy(&mut self, pkg_name: String, version: VersionString) {
+        self.dependencies.insert(pkg_name, version);
+    }
+
+    fn get_dependencies(&self) -> Vec<(String, String)> {
+        let mut deps = Vec::new();
+        for (key, version) in &self.dependencies {
+            deps.push((key.to_owned(), version.to_specific_version()))
+        }
+        deps
+    }
+
+    fn get_dev_dependencies(&self) -> Vec<(String, String)> {
+        let mut deps = Vec::new();
+        if let Some(dev_deps) = &self.dev_dependecies {
+            for (key, version) in dev_deps {
+                deps.push((key.to_owned(), version.to_specific_version()))
+            }
+        }
+        deps
+    }
+}
+
+#[cfg(test)]
+mod package_json_test {
+    use crate::packge_json::VersionString;
+
+    use super::Package;
+    use serde_json::{self, Value};
+    use std::fs::{self, File};
+
+    #[test]
+    fn read_file() {
+        // let p = OpenOptions::new().read(true).open("./package.json");
+        let text = fs::read_to_string("./package.json").unwrap();
+        let mut package: Package = serde_json::from_str(&text).unwrap();
+        println!("{:?}\n\n\n", package);
+        package.add_dependecy(
+            "socket-store".to_owned(),
+            VersionString::new("^0.1.0".to_owned()),
+        );
+
+        package.save();
+        println!("{:?}", package);
+    }
+}
