813db99e794522957b06ebf56326edf73919a8a4
nerdchanii <nerdchanii@gmail.com>
2023-03-17 00:10:34 +0900

    feat(install): implement install command

--- src/lib/command/working_process/install.rs
+++ src/lib/command/working_process/install.rs
@@ -1,9 +1,27 @@
 use std::time::Duration;
 use tokio::time::sleep;
 
+use crate::{command::working_process, packge_json::Package};
+
 pub async fn install() -> Result<(), reqwest::Error> {
-    // for make sekeleton of install process with async
+    let pkg_json = Package::read_file();
+    // 1. dependencies 받아와서 install
+    // 2. devDependencies 받아와서 install
+    let dependencies = pkg_json.get_dependencies();
+    let libs = dependencies
+        .iter()
+        .map(|(lib_name, version)| format!("{}@{}", lib_name, version))
+        .collect::<Vec<String>>();
+    working_process::add(libs, false).await?;
+
+    let dev_deps = pkg_json.get_dev_dependencies();
+    let dev_libs = dev_deps
+        .iter()
+        .map(|(lib_name, version)| format!("{}@{}", lib_name, version))
+        .collect::<Vec<String>>();
+    working_process::add(dev_libs, true).await?;
+
+    // println!("{:?}", pkg_json.get_dev_dependencies());
     sleep(Duration::from_secs(3.to_owned())).await;
-    println!("install process");
     Ok(())
 }
