ebce54460d79f56b7b6b577c4516e590259f9cd9
nerdchanii <nerdchanii@gmail.com>
2023-03-31 14:05:21 +0900

    feat(script): modify scripts

--- scripts/installation.bash.sh
+++ scripts/installation.bash.sh
@@ -1,5 +1,5 @@
-# zsh scripts
-mkdir ~/.rpm &&
-tar -xf rpm.tar.gz -C ~/.rpm &&
-echo "alias rpm="~/.rpm/rpm"" >> ~/.bashrc &&
-echo "rpm has been installed successfully" &&
+# bash scripts
+mkdir ~/.rpm ;
+tar -xf rpm.tar.gz -C ~/.rpm ;
+echo "alias rpm="~/.rpm/rpm"" >> ~/.bashrc ;
+echo "rpm has been installed successfully" ;
--- scripts/installation.zsh.sh
+++ scripts/installation.zsh.sh
@@ -1,7 +1,7 @@
 # zsh scripts
-mkdir ~/.rpm &&
-tar -zxf rpm.tar.gz -C ~/.rpm &&
-echo "alias rpm="~/.rpm/rpm"" >> ~/.zshrc &&
-echo "rpm has been installed successfully" &&
-source ~/.zshrc &&
+mkdir ~/.rpm ;
+tar -zxf rpm.tar.gz -C ~/.rpm ;
+echo "alias rpm="~/.rpm/rpm"" >> ~/.zshrc ;
+echo "rpm has been installed successfully" ;
+source ~/.zshrc > /dev/null 2>&1;
 cd .
